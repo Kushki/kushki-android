b4d339f1eed74345645963a2c7ba5dbe7367d8f0
Diego Villacís <diegovillacis101@gmail.com>
2016-09-30 18:27:32 -0500

    [KFE-203] Fix initialize android script

--- pipeline/initialize-android.sh
+++ pipeline/initialize-android.sh
@@ -9,9 +9,8 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
   download-android
   echo y | android update sdk --no-ui --filter tools,platform-tools
   echo y | android update sdk --no-ui --filter build-tools-24.0.3 --all
-  echo y | android update sdk --no-ui --filter android-15
+  echo y | android update sdk --no-ui --filter android-22
   echo y | android update sdk --no-ui --filter extra-google-m2repository --all
   echo y | android update sdk --no-ui --filter extra-android-m2repository --all
-  echo y | android update sdk --no-ui --filter sys-img-armeabi-v7a-android-19 --all
   touch ${INITIALIZATION_FILE}
 fi
