fb3f70c19397c92571c5f14295d96e7b8d952d1d
Diego Villacís <diegovillacis101@gmail.com>
2016-09-30 18:07:37 -0500

    [KFE-203] Fix typo

--- pipeline/00-initialize-android.sh
+++ pipeline/00-initialize-android.sh
@@ -8,7 +8,7 @@ INITIALIZATION_FILE="$ANDROID_HOME/.initialized-dependencies-$(git log -n 1 --fo
 if [ ! -e ${INITIALIZATION_FILE} ]; then
   download-android
   echo y | android update sdk --no-ui --filter tools,platform-tools > /dev/null
-  echo y | android update sdk --no-ui --filter x-24.0.3 --all > /dev/null
+  echo y | android update sdk --no-ui --filter build-tools-24.0.3 --all > /dev/null
   echo y | android update sdk --no-ui --filter android-15 > /dev/null
   echo y | android update sdk --no-ui --filter extra-google-m2repository --all > /dev/null
   echo y | android update sdk --no-ui --filter extra-android-m2repository --all > /dev/null
