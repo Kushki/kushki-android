d6970f77ce7f256f23c0294b218c5a97b3a131dc
Jonathan Reyes <j@jreyes.org>
2016-10-05 15:54:54 -0500

    [KFE-203] Move library files to the new lib module

--- app/src/main/java/com/kushkipagos/android/MainActivity.java
+++ app/src/main/java/com/kushkipagos/android/MainActivity.java
@@ -6,8 +6,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String MESSAGE = "Eum ipsum eum minima non quasi quos ut aut. Praesentium sint nisi illo et est id reprehenderit. Harum ducimus aperiam ut quod numquam. Ut ipsam nulla ratione.";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +14,5 @@ public class MainActivity extends AppCompatActivity {
         TextView textField = (TextView) findViewById(R.id.txtEncryptedMessage);
         ExampleAsyncTask exampleAsyncTask = new ExampleAsyncTask(textField, kushkiOld);
         exampleAsyncTask.execute("https://ping.aurusinc.com/kushki/api/v1/tokens");
-    }
-
-    private void printEncryptedMessage(TextView textField) {
-        try {
-            AurusEncryption aurusEncryption = new AurusEncryption();
-            textField.setText(aurusEncryption.encryptMessageChunk(MESSAGE));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
