0b1c66644b328d5c9b5fe33aac86b0a890455a33
Jonathan Reyes <j@jreyes.org>
2016-10-07 15:47:16 -0500

    [KFE-203] Write distribution script for the pipeline

--- pipeline/04-distribute.sh
+++ pipeline/04-distribute.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-# source pipeline/initialize-android.sh
-echo Not ready yet
+source pipeline/initialize-android.sh
+artifact_version=$(./gradlew --quiet lib:printVersion)
+git tag --annotate "v$artifact_version" -m "Release for version $artifact_version"
+git push --tags
+./gradlew lib:clean lib:bintrayUpload
+
+# ENVIRONMENT VARIABLES:
+# STAGE
+#  BINTRAY_USER
+#  BINTRAY_API_KEY
+#  MAVEN_CENTRAL_USER
+#  MAVEN_CENTRAL_TOKEN
