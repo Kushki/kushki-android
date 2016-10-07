a46857a5fee39590d9ca97e9ddc5ab27d7515655
Jonathan Reyes <j@jreyes.org>
2016-10-07 12:41:01 -0500

    [KFE-203] Add placeholders for pipeline scripts

---
+++ pipeline/02-integration.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+# We haven't figured out a way to separate integration tests yet
+# source pipeline/initialize-android.sh
+
+echo Not ready yet
---
+++ pipeline/04-distribute.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+# source pipeline/initialize-android.sh
+echo Not ready yet
