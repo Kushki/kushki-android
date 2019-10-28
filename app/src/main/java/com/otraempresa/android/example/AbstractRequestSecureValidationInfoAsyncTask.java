fb4bdb6fad6ad8d17e1322fade56ecd6e5288009
Alex <alex@kushkipagos.com>
2019-10-28 16:55:43 -0500

    [PM-0] Merge

--- app/src/main/java/com/otraempresa/android/example/AbstractRequestSecureValidationInfoAsyncTask.java
+++ app/src/main/java/com/otraempresa/android/example/AbstractRequestSecureValidationInfoAsyncTask.java
@@ -17,7 +17,7 @@ abstract class AbstractRequestSecureValidationInfoAsyncTask extends AsyncTask<As
 
     AbstractRequestSecureValidationInfoAsyncTask(Context context) {
         this.context = context;
-        kushki = new Kushki("20000000107415376000", "COP", KushkiEnvironment.TESTING);
+        kushki = new Kushki("20000000102183993000", "COP", KushkiEnvironment.QA);
     }
 
     protected SecureValidation doInBackground(AskQuestionnaire... args) {
